d4f857d315b27975f4a8f61bf3ef5fab72dd08d5
Dimitris Tsiakmakis <dimatsiak@gmail.com>
2021-03-22 15:18:41 +0200

    utils module added

---
+++ main.py
@@ -0,0 +1,7 @@
+import numpy as np
+import pandas as pd
+
+from utils import load_dataset
+
+employees = load_dataset("csv_name file")
+print(employees.shape)
---
+++ utils.py
@@ -0,0 +1,10 @@
+import numpy as np
+import pandas as pd
+def load_dataset(csv_name):
+    """
+    load the dataset with 'csv_name' in the current directory with intended preprocessing.
+    - return a numpy array (n,m)
+    """
+    employees = pd.read_csv("aug_train.csv")
+    return employees.to_numpy()
+    
