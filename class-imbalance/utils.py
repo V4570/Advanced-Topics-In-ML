eeb3baa05380454c3f2744bfab18cbe5df50f3c9
v4570 <vasilis.tos@gmail.com>
2021-03-22 19:55:27 +0200

    Put data into folder. Added test dataset.

--- class-imbalance/utils.py
+++ class-imbalance/utils.py
@@ -7,5 +7,5 @@ def load_dataset(csv_name):
     load the dataset with 'csv_name' in the current directory with intended preprocessing.
     - return a numpy array (n,m)
     """
-    employees = pd.read_csv("aug_train.csv")
+    employees = pd.read_csv("data/aug_train.csv")
     return employees.to_numpy()
