328deaa9607bc349644135f25a54f3e79e2964de
v4570 <vasilis.tos@gmail.com>
2021-03-25 21:25:15 +0200

    Data preprocessing.

--- class-imbalance/data_preprocessing.py
+++ class-imbalance/data_preprocessing.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import numpy as np
 from sklearn.impute import KNNImputer
+from sklearn.preprocessing import RobustScaler
 
 
 def get_numeric_categorical_cols(df: pd.DataFrame):
@@ -17,24 +18,53 @@ def get_numeric_categorical_cols(df: pd.DataFrame):
 def preprocess_data(filepath):
 	df = pd.read_csv(filepath)
 	
+	# set the target column as our y
 	y = df['target']
 	
+	# drop enrollee_id as it doesn't have any relevance
 	df.drop(['enrollee_id', 'target'], axis=1, inplace=True)
-	# df.replace({'Male': 0, 'Female': 1, 'Other': 3}, inplace=True)
 	
+	# get the names of numeric and categorical columns
 	df_num_cols, df_cat_cols = get_numeric_categorical_cols(df)
 	
+	# normalize numeric columns
+	robust_scaler = RobustScaler()
 	df_num = df[df_num_cols].copy()
+	# create a dataframe from the normalized output and store it back to the variable
+	df_num = pd.DataFrame(robust_scaler.fit_transform(df_num), columns=df_num_cols)
 	
 	df_cat = df[df_cat_cols].copy()
+	# get the names of the columns that have at least one NaN value
 	missing_cat_cols = df_cat.columns[df_cat.isna().any()].tolist()
 	
+	# convert categorical to numeric
 	missing_cat = df_cat[missing_cat_cols].apply(lambda c: c.astype('category').cat.codes)
+	# because when converting to categorical NaN gets mapped to -1 replace it with nan
+	# so that the imputer can work it's magic
 	missing_cat.replace({-1: np.nan}, inplace=True)
+	df_cat.drop(missing_cat_cols, axis=1, inplace=True)
 	
-	knn_imputer = KNNImputer()
+	# normalize the now numeric, categorical columns
+	missing_cat = robust_scaler.fit_transform(missing_cat)
+	
+	# fill missing values
+	knn_imputer = KNNImputer(n_neighbors=3)
 	missing_cat_impute = knn_imputer.fit_transform(missing_cat)
 	
 	missing_cat = pd.DataFrame(missing_cat_impute, columns=missing_cat_cols)
+	# concatenate categorical values
+	df_cat = pd.concat([df_cat, missing_cat], axis=1)
 	
-	x = pd.concat(df_num, )
+	# construct the x dataframe
+	x = pd.concat([df_num, df_cat], axis=1)
+	
+	# save to dataframe to file
+	pd.concat([x, y], axis=1).to_csv('data/processed.csv')
+	
+	return x, y
+
+
+def read_preprocessed(filepath):
+	df = pd.read_csv(filepath)
+	
+	return df.drop('target', axis=1), df['target']
--- class-imbalance/main.py
+++ class-imbalance/main.py
@@ -1,11 +1,18 @@
-from data_preprocessing import preprocess_data
+from data_preprocessing import preprocess_data, read_preprocessed
 from pathlib import Path
+
+PREPROCESS = False
 
 
 def main():
 	datapath = Path("data")
 	
-	preprocess_data(datapath / "aug_train.csv")
+	if PREPROCESS:
+		x, y = preprocess_data(datapath / "aug_train.csv")
+	else:
+		x, y = read_preprocessed(datapath / "processed.csv")
+	
+	print(x.shape, y.shape)
 
 
 if __name__ == '__main__':
