d3809deaefaac2205397b149f7f3927cbbec1494
Dimitris Tsiakmakis <dimatsiak@gmail.com>
2021-03-26 22:13:53 +0200

    diff models experiments

--- class-imbalance/easy_ensemble.py
+++ class-imbalance/easy_ensemble.py
@@ -1,31 +1,41 @@
-from utils import load_dataset
+from data_preprocessing import read_preprocessed, preprocess_data
 import numpy as np
 import pandas as pd
-from collections import Counter
 from sklearn.model_selection import train_test_split
-from sklearn.metrics import confusion_matrix
 from sklearn.metrics import f1_score, accuracy_score
 from imblearn.ensemble import EasyEnsembleClassifier
-from sklearn.datasets import make_classification
-from sklearn.metrics import precision_recall_curve
-from sklearn.metrics import auc
+from sklearn.ensemble import AdaBoostClassifier
+from sklearn.metrics import precision_recall_curve, auc
 from matplotlib import pyplot
+from sklearn import svm
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.metrics import precision_recall_fscore_support
 
-X, y = make_classification(n_classes=2, class_sep=2, weights=[0.80, 0.20], n_samples=5000, random_state=10, n_features=100)
-print('Original dataset shape %s' % Counter(y))
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.1, random_state=0)
+#X, y = preprocess_data("./data/aug_train.csv")
+X, y = read_preprocessed("./data/processed.csv")
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=0)
 
-model = EasyEnsembleClassifier(random_state=42, sampling_strategy="majority", replacement=True, n_jobs=-1, n_estimators=10)
-model.fit(X_train, y_train)
+#Set machine learning model
+knn = KNeighborsClassifier(n_neighbors=2)
+svm = svm.SVC()
+adaboost = AdaBoostClassifier()
+model = EasyEnsembleClassifier(base_estimator=knn, n_estimators=1, sampling_strategy="majority", replacement=True, random_state=42, n_jobs=-1)
+model.fit(X_train, y_train) 
 
+#y_test = 1 - y_test
+
+#Calculate scores
 lr_probs = model.predict_proba(X_test)
 lr_probs = lr_probs[:, 1]
 yhat = model.predict(X_test)
 lr_precision, lr_recall, _ = precision_recall_curve(y_test, lr_probs)
-lr_f1, lr_auc = f1_score(y_test, yhat), auc(lr_recall, lr_precision)
-print('EasyEnsemble: f1=%.3f auc=%.3f' % (lr_f1, lr_auc))
+lr_f1, lr_auc = f1_score(y_test, yhat, average='micro'), auc(lr_recall, lr_precision)
+print('EasyEnsemble: f1 = %.2f%% auc = %.2f%%' % (lr_f1*100, lr_auc*100))
+prec, rec, f1, _ = precision_recall_fscore_support(y_test, yhat, average='micro')
+print('EasyEnsemble: precision = %.2f%% recall = %.2f%%' % (prec*100, rec*100))
 no_skill = len(y_test[y_test==0]) / len(y_test)
 
+#Precision - Recall Curve Plot
 pyplot.plot([0, 1], [no_skill, no_skill], linestyle='--', label='No Skill')
 pyplot.plot(lr_recall, lr_precision, marker='.', label='EasyEnsemble')
 pyplot.xlabel('Recall')
