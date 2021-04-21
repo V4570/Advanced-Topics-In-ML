a35a466491c36a8954f7ed0c775e4e993fcaebfa
Dimitris Tsiakmakis <dimatsiak@gmail.com>
2021-04-22 01:34:51 +0300

    dataset changed

--- multi-label/data_preprocessing.py
+++ multi-label/data_preprocessing.py
@@ -19,72 +19,51 @@ def get_numeric_categorical_cols(df: pd.DataFrame):
 def preprocess_data(filepath):
 	df = pd.read_csv(filepath)
 
-	#df = df.loc[:10000]
+	print(df.isna().any())
 
-	cols = df.columns
-	for col in cols:
-		#find columns which nan values are more than threshold (10%)
-		nan_percentage = df[col].isnull().sum()/len(df.index)*100
-		if(nan_percentage > 10):
-			print("name that droped: " + col + ", nan percentage of column is : " + str(nan_percentage))
-			df.drop(col, axis=1, inplace=True)
-
-
-	#drop columns which are irrelevant (original_title it's almost the same with title).
-	df.drop(['imdb_title_id', 'date_published', 'original_title'], axis=1, inplace=True)
-
-
-	nums, cats = get_numeric_categorical_cols(df)
-	print("total index[before dropna in categorical columns] " + str(len(df.index)))
-	df.dropna(subset=cats, inplace=True)
-	print("total index [after dropna in categorical columns]: " + str(len(df.index)))
+	#drop columns which are irrelevant
+	df.drop(['ID'], axis=1, inplace=True)
 
 	#y data, DONE!
-	y = df['genre'].str.get_dummies(sep=",")
-	df.drop(['genre'], axis=1, inplace=True)
+	y = df.iloc[:,2:]
+	df.drop(['Computer Science','Physics', 'Mathematics', 'Statistics', 'Quantitative Biology', 'Quantitative Finance'], axis=1, inplace=True)
 
-	#fill with mean value where is nan in 'reviews_from_users' feature
-	df['reviews_from_users'].fillna(df['reviews_from_users'].mean(), inplace=True)
-	#print(df.isna().any())
+	#TITLE feature to word_tokenizer!
+	df_title = df['TITLE']
+	df.drop(['TITLE'], axis=1, inplace=True)
+
+	nltk.download('stopwords')
+	nltk.download('punkt')	
+	nltk_stop_words = stopwords.words("english")
 	
-	nums, cats = get_numeric_categorical_cols(df)
-	df_cats = df[cats]
-	df.drop(cats, axis=1, inplace=True)
+	n_components = 250
+	#tfidf_vectoriser = HashingVectorizer(n_features=n_components, stop_words=nltk_stop_words)
+	#tfidf_vectoriser = CountVectorizer(min_df=5, stop_words=nltk_stop_words,ngram_range=(2,2))
+	tfidf_vectoriser = TfidfVectorizer(min_df=5, stop_words=nltk_stop_words,ngram_range=(2,2))
+	# #description data, DONE!
+	title = tfidf_vectoriser.fit_transform(df_title).todense()
+	pca = PCA(n_components=n_components)
+	df_title_feature_names = []
+	for i in range(n_components):
+		df_title_feature_names.append("title_"+str(i))
+	df_title_new = pd.DataFrame(pca.fit_transform(title), columns=df_title_feature_names)
+	print(f"Explained variance for n components [TITLE]: {pca.explained_variance_ratio_[:n_components].sum():.4f}")
 
-	#numeric data, DONE!
-	scaler = MinMaxScaler()
-	df_num = pd.DataFrame(scaler.fit_transform(df), columns=nums)
+	df_abstract = df['ABSTRACT']
+	df.drop(['ABSTRACT'], axis=1, inplace=True)
+	n_components = 3000
+	tfidf_vectoriser = HashingVectorizer(n_features=n_components, stop_words=nltk_stop_words)
+	#tfidf_vectoriser = TfidfVectorizer(min_df=10, stop_words=nltk_stop_words,ngram_range=(2,2))
+	# #description data, DONE!
+	abstract = tfidf_vectoriser.fit_transform(df_abstract).todense().astype('float32')
+	pca = PCA(n_components=n_components)
+	df_abstract_feature_names = []
+	for i in range(n_components):
+		df_abstract_feature_names.append("abstract_"+str(i))
+	df_abstract_new = pd.DataFrame(pca.fit_transform(abstract), columns=df_abstract_feature_names)
+	print(f"Explained variance for n components [ABSTRACT]: {pca.explained_variance_ratio_[:n_components].sum():.4f}")
 
-	#description feature to word_tokenizer!
-	df_description = df_cats['description']
-	df_cats.drop(['description'], axis=1, inplace=True)
-
-	# nltk.download('stopwords')
-	# nltk.download('punkt')	
-	# nltk_stop_words = stopwords.words("english")
-	
-	# n_components = 25
-	# tfidf_vectoriser = HashingVectorizer(n_features=n_components, stop_words=nltk_stop_words)
-	# #tfidf_vectoriser = CountVectorizer(min_df=5, stop_words=nltk_stop_words,ngram_range=(2,2))
-	# #tfidf_vectoriser = TfidfVectorizer(min_df=10, stop_words=nltk_stop_words,ngram_range=(2,2))
-	# # #description data, DONE!
-	# x_t = tfidf_vectoriser.fit_transform(df_description).todense()
-	# pca = PCA(n_components=n_components)
-	# df_description_feature_names = []
-	# for i in range(n_components):
-	# 	df_description_feature_names.append("description_"+str(i))
-	# df_description_new = pd.DataFrame(pca.fit_transform(x_t), columns=df_description_feature_names)
-	# print(f"Explained variance for n components: {pca.explained_variance_ratio_[:n_components].sum():.4f}")
-
-
-	#categories to codes.
-	df_cats = df_cats.apply(lambda c: c.astype('category').cat.codes)
-
-	df_num.reset_index(drop=True, inplace=True)
-	df_cats.reset_index(drop=True, inplace=True)
-	#df_description_new.reset_index(drop=True, inplace=True)
-
-	x = pd.concat([df_num, df_cats], axis=1).astype('float32')
+	x = pd.concat([df_title_new, df_abstract_new], axis=1).astype('float32')
 	#x = pd.concat([df_num, df_cats, df_description_new], axis=1).astype('float32')
 	pd.concat([x, y], axis=1).to_csv('data/processed.csv')
 
@@ -93,13 +72,7 @@ def preprocess_data(filepath):
 	return x, y
 
 def get_labels():
-	return ([' Action', ' Adventure', ' Animation', ' Biography', ' Comedy',
-       ' Crime', ' Drama', ' Family', ' Fantasy', ' Film-Noir', ' History',
-       ' Horror', ' Music', ' Musical', ' Mystery', ' Romance', ' Sci-Fi',
-       ' Sport', ' Thriller', ' War', ' Western', 'Action', 'Adventure',
-       'Animation', 'Biography', 'Comedy', 'Crime', 'Drama', 'Family',
-       'Fantasy', 'Film-Noir', 'History', 'Horror', 'Music', 'Musical',
-       'Mystery', 'Romance', 'Sci-Fi', 'Thriller', 'War', 'Western'])
+	return (['Computer Science','Physics', 'Mathematics', 'Statistics', 'Quantitative Biology', 'Quantitative Finance'])
 
 def read_preprocessed(filepath):
 	df = pd.read_csv(filepath, dtype='float32')
--- multi-label/rakeld.py
+++ multi-label/rakeld.py
@@ -1,14 +1,32 @@
 from sklearn.naive_bayes import GaussianNB
 from skmultilearn.ensemble import RakelD
-from data_preprocessing import read_preprocessed
+from data_preprocessing import read_preprocessed, preprocess_data
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import precision_recall_fscore_support, accuracy_score
+from sklearn.metrics import average_precision_score, f1_score
+import numpy as np
+from sklearn.preprocessing import MultiLabelBinarizer
 
-X, y = read_preprocessed("./data/processed.csv")
+def rakeldClassifier(x, y, test_size, clf):
+    classifier = RakelD(
+        base_classifier=clf
+    )
+    X_train, X_test, y_train, y_test = train_test_split(x, y, test_size=test_size, random_state=0)
+    classifier.fit(X_train, y_train)
+    yhat = classifier.predict(X_test)
+    acc = accuracy_score(y_test, yhat)
+    prec, rec, f1, _ = precision_recall_fscore_support(y_test, yhat, average='micro')
+    print('RakelD: f1 = %.2f%%, acc = %.2f%%' % (f1*100, acc*100))
+    f1_s = f1_sampled(y_test.to_numpy(), yhat.toarray())
+    print('RakelD: f1_sampled = %.2f%%' % (f1_s*100))
 
-classifier = RakelD(
-    base_classifier=GaussianNB(),
-    base_classifier_require_dense=[True, True],
-    labelset_size=4
-)
+def f1_sampled(actual, pred):
+    
+    #converting the multi-label classification to a binary output
+    mlb = MultiLabelBinarizer()
+    actual = mlb.fit_transform(actual)
+    pred = mlb.fit_transform(pred)
 
-classifier.fit(X_train, y_train)
-prediction = classifier.predict(X_train, y_train)
+    #fitting the data for calculating the f1 score 
+    f1 = f1_score(actual, pred, average = "samples")
+    return f1
