03be9becde6de18c6657573f27dd82590800dfc7
v4570 <vasilis.tos@gmail.com>
2021-06-16 02:12:15 +0300

    Run multiple classifiers and store results in dictionary.

--- class-imbalance/easy_ensemble.py
+++ class-imbalance/easy_ensemble.py
@@ -5,7 +5,7 @@ from matplotlib import pyplot
 from sklearn.metrics import precision_recall_fscore_support
 
 
-def easy_ensemble(x_train, x_test, y_train, y_test, classifier):
+def easy_ensemble(x_train, x_test, y_train, y_test, classifier, standalone=False):
 	
 	model = EasyEnsembleClassifier(
 		base_estimator=classifier,
@@ -16,22 +16,26 @@ def easy_ensemble(x_train, x_test, y_train, y_test, classifier):
 	)
 	model.fit(x_train, y_train)
 	
-	# Calculate scores
-	lr_probs = model.predict_proba(x_test)
-	lr_probs = lr_probs[:, 1]
 	yhat = model.predict(x_test)
-	lr_precision, lr_recall, _ = precision_recall_curve(y_test, lr_probs)
-	lr_f1, lr_auc = f1_score(y_test, yhat, average='micro'), auc(lr_recall, lr_precision)
-	print('EasyEnsemble: f1 = %.2f%% auc = %.2f%%' % (lr_f1*100, lr_auc*100))
-	prec, rec, f1, _ = precision_recall_fscore_support(y_test, yhat, average='micro')
-	print('EasyEnsemble: precision = %.2f%% recall = %.2f%%' % (prec*100, rec*100))
-	no_skill = len(y_test[y_test==0]) / len(y_test)
 	
-	# Precision - Recall Curve Plot
-	pyplot.plot([0, 1], [no_skill, no_skill], linestyle='--', label='No Skill')
-	pyplot.plot(lr_recall, lr_precision, marker='.', label='EasyEnsemble')
-	pyplot.xlabel('Recall')
-	pyplot.ylabel('Precision')
-	pyplot.legend()
-	pyplot.title("Precision-Recall Curve")
-	pyplot.show()
+	if standalone:
+		# Calculate scores
+		lr_probs = model.predict_proba(x_test)
+		lr_probs = lr_probs[:, 1]
+		lr_precision, lr_recall, _ = precision_recall_curve(y_test, lr_probs)
+		lr_f1, lr_auc = f1_score(y_test, yhat, average='micro'), auc(lr_recall, lr_precision)
+		print('EasyEnsemble: f1 = %.2f%% auc = %.2f%%' % (lr_f1 * 100, lr_auc * 100))
+		prec, rec, f1, _ = precision_recall_fscore_support(y_test, yhat, average='micro')
+		print('EasyEnsemble: precision = %.2f%% recall = %.2f%%' % (prec * 100, rec * 100))
+		no_skill = len(y_test[y_test == 0]) / len(y_test)
+		
+		# Precision - Recall Curve Plot
+		pyplot.plot([0, 1], [no_skill, no_skill], linestyle='--', label='No Skill')
+		pyplot.plot(lr_recall, lr_precision, marker='.', label='EasyEnsemble')
+		pyplot.xlabel('Recall')
+		pyplot.ylabel('Precision')
+		pyplot.legend()
+		pyplot.title("Precision-Recall Curve")
+		pyplot.show()
+	
+	return yhat
