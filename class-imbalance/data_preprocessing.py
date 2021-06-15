Fix scaling on whole dataset instead of splitting into training and testing first in class imbalance project.
Changes to all methods to match how we handle the data now.

class-imbalance/data_preprocessing.py
@@ -1,6 +1,6 @@
 import pandas as pd
 import numpy as np
-from sklearn.impute import KNNImputer
+from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import MinMaxScaler
 
 
@@ -15,7 +15,7 @@ def get_numeric_categorical_cols(df: pd.DataFrame):
 	return df.select_dtypes(include=numerics).columns, df.select_dtypes(include=object).columns
 
 
-def preprocess_data(filepath):
+def preprocess_data(filepath, test_size):
 	df = pd.read_csv(filepath)
 	
 	# set the target column as our y
@@ -27,49 +27,68 @@ def preprocess_data(filepath):
 	# get the names of numeric and categorical columns
 	df_num_cols, df_cat_cols = get_numeric_categorical_cols(df)
 	
-	# normalize numeric columns
-	scaler = MinMaxScaler()
-	df_num = df[df_num_cols].copy()
-	# create a dataframe from the normalized output and store it back to the variable
-	df_num = pd.DataFrame(scaler.fit_transform(df_num), columns=df_num_cols)
+	x_train, x_test, y_train, y_test = train_test_split(df, y, test_size=test_size, random_state=0)
 	
-	df_cat = df[df_cat_cols].copy()
+	scaler = MinMaxScaler()
+	
+	train_num = x_train[df_num_cols].copy()
+	train_num = pd.DataFrame(scaler.fit_transform(train_num), columns=df_num_cols, index=train_num.index)
+	
+	test_num = x_test[df_num_cols].copy()
+	test_num = pd.DataFrame(scaler.transform(test_num), columns=df_num_cols, index=test_num.index)
+	
+	train_cat, missing_train_cat, missing_train_cols = format_cat_cols(x_train[df_cat_cols].copy())
+	test_cat, missing_test_cat, missing_test_cols = format_cat_cols(x_test[df_cat_cols].copy())
+	
+	missing_train_cat = pd.DataFrame(
+		scaler.fit_transform(missing_train_cat),
+		columns=missing_train_cols,
+		index=missing_train_cat.index
+	)
+	missing_train_cat = missing_train_cat.ffill(axis=1).bfill(axis=1)
+	
+	missing_test_cat = pd.DataFrame(
+		scaler.transform(missing_test_cat),
+		columns=missing_test_cols,
+		index=missing_test_cat.index
+	)
+	missing_test_cat = missing_test_cat.ffill(axis=1).bfill(axis=1)
+	
+	train_cat = pd.concat([train_cat, missing_train_cat], axis=1)
+	test_cat = pd.concat([test_cat, missing_test_cat], axis=1)
+	
+	x_train = pd.concat([train_num, train_cat], axis=1)
+	x_test = pd.concat([test_num, test_cat], axis=1)
+	
+	pd.concat([x_train, y_train], axis=1).to_csv('data/processed_train.csv')
+	pd.concat([x_test, y_test], axis=1).to_csv('data/processed_test.csv')
+	
+	return x_train, x_test, y_train, y_test
+
+
+def format_cat_cols(df):
+	
 	# convert categorical to numeric
-	df_cat = df_cat.apply(lambda c: c.astype('category').cat.codes)
+	df = df.apply(lambda c: c.astype('category').cat.codes)
 	
 	# because when converting to categorical NaN gets mapped to -1 replace it with nan
 	# so that the imputer can work it's magic
-	df_cat.replace({-1: np.nan}, inplace=True)
+	df.replace({-1: np.nan}, inplace=True)
+	
 	# get the names of the columns that have at least one NaN value
-	missing_cat_cols = df_cat.columns[df_cat.isna().any()].tolist()
-	
+	missing_cat_cols = df.columns[df.isna().any()].tolist()
 	# get only the columns with null values
-	missing_cat = df_cat[missing_cat_cols]
 	
-	df_cat.drop(missing_cat_cols, axis=1, inplace=True)
+	missing_cat = pd.DataFrame(df[missing_cat_cols], columns=missing_cat_cols, index=df[missing_cat_cols].index)
 	
-	# normalize the now numeric, categorical columns
-	missing_cat = pd.DataFrame(scaler.fit_transform(missing_cat), columns=missing_cat_cols)
-	missing_cat = missing_cat.ffill(axis=1).bfill(axis=1)
-	
-	# fill missing values
-	# knn_imputer = KNNImputer(n_neighbors=3)
-	# missing_cat_impute = knn_imputer.fit_transform(missing_cat)
-	
-	# missing_cat = pd.DataFrame(missing_cat_impute, columns=missing_cat_cols)
-	# concatenate categorical values
-	df_cat = pd.concat([df_cat, missing_cat], axis=1)
-	
-	# construct the x dataframe
-	x = pd.concat([df_num, df_cat], axis=1)
-	
-	# save to dataframe to file
-	pd.concat([x, y], axis=1).to_csv('data/processed.csv')
-	
-	return x, y
+	df.drop(missing_cat_cols, axis=1, inplace=True)
+
+	return df, missing_cat, missing_cat_cols
 
 
 def read_preprocessed(filepath):
-	df = pd.read_csv(filepath)
+	train_df = pd.read_csv(filepath/'processed_train.csv')
+	test_df = pd.read_csv(filepath/'processed_test.csv')
 	
-	return df.drop('target', axis=1), df['target']
+	# x_train, x_test, y_train, y_test
+	return train_df.drop('target', axis=1), test_df.drop('target', axis=1), train_df['target'], test_df['target']

class-imbalance/no_sampling.py
@@ -1,12 +1,10 @@
 from sklearn.metrics import f1_score, precision_recall_curve, precision_recall_fscore_support, auc
-from sklearn.model_selection import train_test_split
 from matplotlib import pyplot as plt
 import numpy as np
 
 
-def no_sampling(x, y, test_size, classifier):
-	x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=test_size, random_state=0)
-	
+def no_sampling(x_train, x_test, y_train, y_test, classifier):
+
 	clf = classifier
 	clf.fit(x_train, y_train)
 	
@@ -31,12 +29,12 @@ def no_sampling(x, y, test_size, classifier):
 	plt.title("Precision-Recall Curve")
 	plt.show()
 
-def only_majority_class(x, y, test_size, classifier):
+
+def only_majority_class(x_train, x_test, y_train, y_test, classifier):
 	'''
 	classifier is dump variable, only for reasons of uniformity. 
 	Function classifies data into the majority class.
 	'''
-	x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=test_size, random_state=0)
 	
 	y_pred = np.zeros(y_test.shape[0])
 	