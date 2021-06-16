2f87e72e93d01f330c753daed988370f0303dffc
Dogoulis <pantelisdog@gmail.com>
2021-06-16 22:52:49 +0300

    minor changes (not using the f1_sample metric)

--- multi-label/label_powerset.py
+++ multi-label/label_powerset.py
@@ -13,9 +13,10 @@ def label_powerset(x_train, x_test, y_train, y_test, clf, standalone=False):
 	if standalone:
 		acc = accuracy_score(y_test, y_pred)
 		prec, rec, f1, _ = precision_recall_fscore_support(y_test, y_pred, average='micro')
-		f1_s = f1_sampled(y_test.to_numpy(), y_pred.toarray())
+		# f1_s = f1_sampled(y_test.to_numpy(), y_pred.toarray())
 		
 		print('Label powerset: f1 = %.2f%%, acc = %.2f%%' % (f1 * 100, acc * 100))
+		'''
 		print('Label powerset: f1_sampled = %.2f%%' % (f1_s * 100))
 
 
@@ -28,3 +29,4 @@ def f1_sampled(actual, pred):
 	# fitting the data for calculating the f1 score
 	f1 = f1_score(actual, pred, average="samples")
 	return f1
+'''
--- multi-label/rakeld.py
+++ multi-label/rakeld.py
@@ -13,14 +13,14 @@ def rakeld_classifier(x_train, x_test, y_train, y_test, clf, standalone=False):
     if standalone:
         acc = accuracy_score(y_test, yhat)
         prec, rec, f1, _ = precision_recall_fscore_support(y_test, yhat, average='micro')
-        f1_s = f1_sampled(y_test.to_numpy(), yhat.toarray())
+        # f1_s = f1_sampled(y_test.to_numpy(), yhat.toarray())
     
         print('RakelD: f1 = %.2f%%, acc = %.2f%%' % (f1 * 100, acc * 100))
-        print('RakelD: f1_sampled = %.2f%%' % (f1_s * 100))
+        # print('RakelD: f1_sampled = %.2f%%' % (f1_s * 100))
     
     return yhat
 
-
+'''
 def f1_sampled(actual, pred):
     
     # converting the multi-label classification to a binary output
@@ -31,3 +31,4 @@ def f1_sampled(actual, pred):
     # fitting the data for calculating the f1 score
     f1 = f1_score(actual, pred, average="samples")
     return f1
+'''
